befcf7a6b0474678e0adaf5cb1823fd46c8d37c9
eldadtis <eldad.tishler@gmail.com>
2021-10-05 13:29:21 +0300

    add react-bootstrap

---
+++ client-side/src/components/Header.js
@@ -0,0 +1,24 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">EldadShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i> CART
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> SIGN IN
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
