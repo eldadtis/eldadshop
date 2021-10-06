e7824e9f3756761c5ef357272270a8aad5edb084
eldadtis <eldad.tishler@gmail.com>
2021-10-06 15:48:31 +0300

    fetch products from server

--- client-side/src/screens/ProductScreen.js
+++ client-side/src/screens/ProductScreen.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Row,
@@ -9,10 +10,20 @@ import {
   ListGroupItem,
 } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 export default function ProductScreen({ match }) {
-  const product = products.find((p) => p.id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [match.params.id]);
   return (
     <div>
       <Link className="my-3 btn btn-info" to="/">
