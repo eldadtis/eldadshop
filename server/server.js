330651cd37739bd30d5575aa412edb9de8e31f1b
eldadtis <eldad.tishler@gmail.com>
2021-10-09 11:43:03 +0300

    fetch products mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRouter from "./routes/productRouter.js";
 
 dotenv.config();
 
@@ -13,14 +13,7 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p.id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRouter);
 
 const PORT = process.env.PORT || 5000;
 
